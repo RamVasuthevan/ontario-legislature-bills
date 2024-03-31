e4f7b3234c02482e0d9450a03214c9057969dc3d
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2024-03-31 00:01:31 -0400

    Get Bill Statuses (#3)
    * Checkpoint POC works
    
    * Rename
    
    * Complete Statuses
    
    * Update data
    
    * Update ReadMe
    
    * Add sleep
    
    * Add WAIT_SECONDS = 1

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import logging
 import time
 
-from src import bills, log_config, parliaments
+from src import bills, log_config, parliaments, statuses
 
 log_config.configure_logging()
 
-WAIT_SECONDS = 2 
+WAIT_SECONDS = 1
+
 
 def main():
     logging.info(
@@ -15,34 +16,53 @@ def main():
     parliament_html_content = parliaments.extract()
     time.sleep(WAIT_SECONDS)
     parliament_data = parliaments.transform(parliament_html_content)
+    logging.info(
+        f"Parliament data extraction and transformation completed. {len(parliament_data)} records extracted."
+    )
+
     parliaments.load(parliament_data)
     logging.info(
-        f"Parliament data extraction and processing completed. {len(parliament_data)} records loaded."
+        f"Parliament data saved to '{parliaments.CSV_FILENAME}' successfully!"
     )
 
     all_bill_data = []
     logging.info("Starting the extraction process for bill data.")
 
     for parliament in parliament_data:
-        parliament_url = parliament.url
-        parliament_name = parliament.parliament
         logging.info(
-            f"Extracting bill data for {parliament_name} from {parliament_url}"
+            f"Extracting bill data for {parliament.url} from {parliament.url}"
         )
 
-        bill_html_content = bills.extract(parliament_url)
+        bill_html_content = bills.extract(parliament.url)
         time.sleep(WAIT_SECONDS)
-        bill_data = bills.transform(parliament_name, bill_html_content)
+        bill_data = bills.transform(parliament.url, bill_html_content)
         all_bill_data.extend(bill_data)
+    logging.info(
+        f"Bill data extraction and transformation completed. {len(all_bill_data)} records extracted."
+    )
 
     bills.load(all_bill_data)
     logging.info(
-        f"Bill data extraction and processing completed. {len(all_bill_data)} records loaded."
+        f"Detailed bill data saved to '{bills.CSV_FILENAME}' successfully!"
     )
 
-    logging.info(
-        f"Data extracted, processed, and saved to '{parliaments.CSV_FILENAME}' and '{bills.CSV_FILENAME}' successfully!"
-    )
+
+    all_status_data = []
+    logging.info("Starting the extraction process for status data.")
+    for bill in all_bill_data:
+        logging.info(
+            f"Extracting status data for Bill {bill.bill_number}, {bill.bill_title}, {bill.parliament} from {bill.url_title+statuses.STATUS_URL_SUFFIX}"
+        )
+
+        status_html_content = statuses.extract(bill.url_title)
+        time.sleep(WAIT_SECONDS)
+        status_data = statuses.transform(bill.parliament, bill.bill_number, bill.bill_title, status_html_content)
+        all_status_data.extend(status_data)
+    logging.info("Status data extraction and transformation completed. {len(all_status_data)} records extracted.")
+
+    statuses.load(all_status_data)
+    logging.info(f"Status data saved to '{statuses.CSV_FILENAME}' successfully!")
+
 
 
 if __name__ == "__main__":
